f02a09bdf2ca15a9f89b889f15e3dfdc36294ef2
JaydenSzakmary <szakmaryjayden911@gmail.com>
2022-02-09 17:33:54 -0600

    2/9 hw

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,9 +1,9 @@
 from .import bp as main
-from flask import render_template, request
+from flask import render_template, request, flash
 import requests
-from flask_login import  login_required
+from flask_login import  current_user, login_required
 from .forms import SearchForm
-
+from app.models import Pokemon
 
 
 @main.route('/')
@@ -24,21 +24,68 @@ def pokedex():
         if response.ok:
             data = response.json()
             new_data = []
-            print(new_data)
             poke_dict = {}
-            poke_name=f"{ name }" 
             poke_dict={
                 "form_name": data['forms'][0]['name'], 
                 "abilities": data['abilities'][0]['ability']['name'],
-                "base_xp": data['base_experience'],
+                "base_hp": data['stats'][0]['base_stat'],
                 "sprite_url" : data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon']['front_shiny'],
                 }
             new_data.append(poke_dict)
-            print(new_data)
+            try:
+                new_poke_data = Pokemon()
+                new_poke_data.from_dict(poke_dict)
+                new_poke_data.save_to_db()
+                flash("Pokemon added to local Pokedex!",'success')
+            except:
+                flash("Pokemon already in local Pokedex!",'warning') 
             return render_template('pokedex.html', pokemon = new_data, form=form)
         else:
             error_msg = f"Pokemon not found! {name} is not registered in the global Pokedex!"
-            return render_template('pokedex.html', error = error_msg)
+            return render_template('pokedex.html',form=form, error = error_msg)
     return render_template('pokedex.html', form=form)
 
+
+@main.route('/pokedex_add', methods = ['GET','POST'])
+@login_required
+def pokedex_add():
+    form = SearchForm()
+    poke = []
+    if request.method == 'POST' and form.validate_on_submit():
+        poke = Pokemon.query.filter_by(name=form.name.data.lower()).first()
+        if poke:
+            
+            print(poke)
+            if len(list(current_user.mons))>=5:
+     
+                current_user.save_mons(poke)
+                flash('pokemon added to party successfully!','success')
+
+            return render_template('pokedex_add.html', poke = poke, form=form)
+        else:
+            error_msg = f"Pokemon not found! is not registered in the global Pokedex!"
+            return render_template('pokedex_add.html',form=form, error = error_msg)
+    return render_template('pokedex_add.html',poke= poke , form=form)
+
+
+@main.route('/pokedex_remove', methods = ['GET','POST'])
+@login_required
+def pokedex_remove():
+    form = SearchForm()
+    poke = []
+    if request.method == 'POST' and form.validate_on_submit():
+        
+        poke = Pokemon.query.filter_by(name=form.name.data.lower()).first()
+        if poke:
+            
+            
+     
+            current_user.remove_mons(poke)
+            flash('pokemon removed from party successfully!','success')
+
+            return render_template('pokedex_remove.html', poke = poke, form=form)
+        else:
+            error_msg = f"Pokemon not found! is not registered in the global Pokedex!"
+            return render_template('pokedex_remove.html',form=form, error = error_msg)
+    return render_template('pokedex_remove.html',poke= poke , form=form)
 
--- app/models.py
+++ app/models.py
@@ -2,15 +2,54 @@ from app import db, login
 from flask_login import UserMixin #for user moddel!
 from datetime import datetime as dt
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship, backref
+
+
+class User_Pokemon(db.Model):
+    __tablename__= 'user_pokemon'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),primary_key=True)
+    poke_id = db.Column(db.Integer, db.ForeignKey('pokemon.poke_id'),primary_key=True)
+    
+
+
+class Pokemon(db.Model):
+    __tablename__= 'pokemon'
+    poke_id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(150),unique=True)
+    abilities = db.Column(db.String(150))
+    base_hp = db.Column(db.Integer)
+    sprite_url = db.Column(db.String(1500))
+    
+
+    def __repr__(self):
+        return f'<Pokemon: {self.poke_id} | {self.name}>'
+
+    def from_dict(self, data):
+        self.name = data['form_name']
+        self.abilities = data['abilities']
+        self.base_hp = data['base_hp']
+        self.sprite_url = data['sprite_url']
+    
+    def save_to_db(self):
+        db.session.add(self)  #add tghe yser ti seession
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(200))
     creation_date = db.Column(db.DateTime, default= dt.utcnow)
+    mons = db.relationship('Pokemon',secondary='user_pokemon',lazy='dynamic',backref='trainer')
 
     def __repr__(self):
         return f'<User: {self.id} | {self.email}>'
@@ -34,7 +73,22 @@ class User(UserMixin, db.Model):
         db.session.add(self)  #add tghe yser ti seession
         db.session.commit()  #save everything in the session to database
 
+    def save_mons(self,poke):
+        self.mons.append(poke)
+        print(self.mons)
+        print('hello')
+        db.session.commit()
+
+    def remove_mons(self,poke):
+        self.mons.remove(poke)
+        print(self.mons)
+        print('hello')
+        db.session.commit()   
+
     #select * from user where id = ??
+
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
